c8ee054df6b3c3f6f903033f10ab74e158ff9762
andrean <andreanpal@gmail.com>
2013-06-14 17:47:37 +0600

    Changed methods readItem, writeItem, _read, _write

--- lemon/lemonServer/basicStorage.py
+++ lemon/lemonServer/basicStorage.py
@@ -4,26 +4,46 @@ Created on 14.06.2013
 @author: vau
 '''
 
+import common.IdGenerator
+
 class Storage(object):
     '''
     Class using different files for every item
     '''
 
 
-    def __init__(self, _uniqueId):
+    def __init__(self, preinitFile):
         '''
         Constructor
         '''
-        self._storageID = _uniqueId;
+        self._storageID =   self._read(preinitFile)
+        if(~self._storageID):                                        
+            self._storageID = common.IdGenerator.GenerateNewUniqueID();
+            self._write(preinitFile, self._storageID)
+        
+    # return byte content from file
     def readItem(self, item_id):
-        pass
+        fileName = self._getFilename(item_id);
+        return self._read(fileName);
     
+    # transform content to byte sequence and write it into file
     def writeItem(self, item_id, content):
-        pass
+        fileName = self._getFilename(item_id);
+        return self._write(fileName, content);
     
     def _read(self, file):
-        pass
+        try:
+            f = open(file, 'rb');
+        except IOError:
+            return;
+        content = f.read()
+        f.close()
+        return content
     
     def _write(self, file, content):
-        pass
-    
+        f = open(file, 'wb')
+        f.write(content)
+            
+    def _getFilename(self, item_id):
+        if not item_id:
+            return self._storageID + item_id;
