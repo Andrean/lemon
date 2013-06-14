3aedf1d47903d6cbaf024f4510a8e9c7f1ca6328
andrean <andreanpal@gmail.com>
2013-06-14 16:24:21 +0600

    Added basic class of storage data in bytes. Structure of storage:  - item_id1: bytecontent  - item_id2: bytecontent

---
+++ lemon/lemonServer/basicStorage.py
@@ -0,0 +1,21 @@
+'''
+Created on 14.06.2013
+
+@author: vau
+'''
+
+class Storage(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    def readItem(self, item_id):
+        pass
+    
+    def writeItem(self, item_id, content):
+        pass
