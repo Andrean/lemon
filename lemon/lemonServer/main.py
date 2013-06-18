e0afe878b68ac30d5ace0db5ffef986a6e769729
andrean <andreanpal@gmail.com>
2013-06-18 13:00:11 +0600

    Added in main module configuration. Added logging file configuration

--- lemon/lemonServer/main.py
+++ lemon/lemonServer/main.py
@@ -5,8 +5,19 @@ Created on 13.06.2013
 '''
 import server
 import webInterfaceListener
+import main_config
+import configparser
+import logging.config
+
 
 if __name__ == '__main__':
+    
+    config  = configparser.ConfigParser()
+    config.read(main_config.CONFIG_FILE)
+    if len(config.sections()) < 1:
+        main_config.writeDefaultConfig(config)
+    logging.config.fileConfig(config['LOGGING']['file'])
+    
     agentServerInstance      = server.Server(20);
     httpInterfaceInstance    = webInterfaceListener.httpListener();
     
