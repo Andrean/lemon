10b3e5bd4aac047276d1487f8b118745db95376c
andrean <andreanpal@gmail.com>
2013-06-18 16:39:55 +0600

    refactored main module

--- lemon/lemonServer/main.py
+++ lemon/lemonServer/main.py
@@ -7,19 +7,23 @@ import server
 import webInterfaceListener
 import main_config
 import configparser
+import logging
 import logging.config
 
 
 if __name__ == '__main__':
     
+    # set up configure 
     config  = configparser.ConfigParser()
     config.read(main_config.CONFIG_FILE)
     if len(config.sections()) < 1:
         main_config.writeDefaultConfig(config)
     logging.config.fileConfig(config['LOGGING']['file'])
     
+    # creating interface instances
     agentServerInstance      = server.Server(20);
     httpInterfaceInstance    = webInterfaceListener.httpListener();
     
+    #starting instances
     agentServerInstance.start();
     httpInterfaceInstance.start();
