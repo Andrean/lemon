016bc40e344d778100c29e3326d17815e365f7d9
andrean <andreanpal@gmail.com>
2013-06-19 12:09:00 +0600

    Added new module agentInterface. This is module for base class with main methods for interface with agents

---
+++ lemon/lemonServer/agentInterface.py
@@ -0,0 +1,17 @@
+'''
+Created on 19.06.2013
+
+@author: vau
+'''
+
+class agentListener(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
