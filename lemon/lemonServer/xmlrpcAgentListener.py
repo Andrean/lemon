1f9731e2b677b35a0c9f061df07c544795663004
andrean <andreanpal@gmail.com>
2013-06-19 19:08:56 +0600

    Changed xmlrpcAgentListener. Excluded AgentListener from base classes.

--- lemon/lemonServer/xmlrpcAgentListener.py
+++ lemon/lemonServer/xmlrpcAgentListener.py
@@ -4,9 +4,14 @@ Created on 19.06.2013
 @author: vau
 '''
 
-from agentInterface import AgentListener
+from agentInterface import AgentHandler
 from socketserver import ThreadingMixIn
 from xmlrpc.server import SimpleXMLRPCServer
+from exception.lemonException import LemonException
 
 class xmlrpcAgentListener(ThreadingMixIn, SimpleXMLRPCServer ):
     pass
+
+class XMLRPCExitException(LemonException):
+    pass
+    
