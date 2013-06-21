9f2245aac230d5dcdb0190bdcf1ba2064d48979f
andrean <andreanpal@gmail.com>
2013-06-21 17:36:47 +0600

    Added public method 'startSession' Method generate new session identifier for connected agent. This identifier is not expired. Its behavior will be changed in close future

--- lemon/lemonServer/agentInterface.py
+++ lemon/lemonServer/agentInterface.py
@@ -4,6 +4,11 @@ Created on 19.06.2013
 @author: vau
 '''
 
+import common.IdGenerator
+import time
+
+SECONDS  = 1
+MINUTES  = 60*SECONDS
 class AgentHandler(object):
     '''
     classdocs
@@ -14,6 +19,22 @@ class AgentHandler(object):
         '''
         Constructor
         '''
+        self._sessionStorage = {}
+        self._expireTime = 20 * MINUTES
+    def startSession(self, _agentId):
+        if _agentId not in self._sessionStorage:
+            _sessionId = common.IdGenerator.GenerateSessionID()
+            self._sessionStorage[_agentId] = {'id':_sessionId, 'expire': time.time() + self._expireTime}
+            return _sessionId
+        return self._sessionStorage[_agentId]['id']
+    
+    def _checkExpiredSessions(self):
+        pass
+            
+    
+    
+    
+    
     
          
     
