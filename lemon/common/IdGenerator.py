3201c600186144f3841ac5a6d5646842ba466df1
andrean <andreanpal@gmail.com>
2013-06-21 18:55:42 +0600

    Added new function GenerateSessionId

--- lemon/common/IdGenerator.py
+++ lemon/common/IdGenerator.py
@@ -11,3 +11,9 @@ random.seed()
 def GenerateNewUniqueID():
     _id = str(int(time.time()) % 1677721) + str(random.randint(1000000,9999999));
     return _id;
+
+def GenerateSessionID():
+    _id = random.randint(1000000,9999999)
+    _id = str(_id) + str(random.randint(1000000,9999999))
+    _id = _id + str(random.randint(1000000,9999999)) 
+    return _id
