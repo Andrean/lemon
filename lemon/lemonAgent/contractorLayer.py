8b033b91aea178d916023bce970dea94cdfc5d28
andrean <andreanpal@gmail.com>
2013-07-10 18:46:10 +0600

    Fixed bugs in Contractor class

--- lemon/lemonAgent/contractorLayer.py
+++ lemon/lemonAgent/contractorLayer.py
@@ -42,6 +42,7 @@ class Layer(lemon.BaseAgentLemon):
     def run(self):
         if not os.path.exists(self.contractors_path):
             os.makedirs(self.contractors_path)
+        self._load()
         self._running   = True
         self._logger.info("Layer started")
         while self._running:
@@ -92,7 +93,7 @@ class Layer(lemon.BaseAgentLemon):
             self._stop(path, args)
     
     def _start(self, k, path, args):
-        c    = Contractor(self._logger, self._config, self._contractors[k], path, args)
+        c    = Contractor(self._logger, self._config, self._contractors[k], self.contractors_path + path, args)
         c.start()
     
     def _stop(self, k, path):
@@ -109,6 +110,7 @@ class Layer(lemon.BaseAgentLemon):
     def _load(self):
         try:
             value   = self._storage.readStr(MASK)
+            print(value)
             if value is not None:
                 self._contractors   = json.loads(value)
         except Exception as e:
@@ -125,15 +127,16 @@ class Contractor(lemon.BaseAgentLemon):
     
     def __init__(self, _logger, _config, _place, path, args):
         self._place = _place
-        self._call_args = list(path).append(args)
-        print(self._call_args)
+        self._call_args = [path]
+        self._call_args.extend(args)
         lemon.BaseAgentLemon.__init__(self, _logger, _config)
         self._logger.info("contractor {0} created".format(self._place['__id']))
     
     def run(self):
         self.running    = True
-        self.out
-        self.err
-        exit_code   = self.subprocces = subprocess.call(self._call_args, stdout = self.out, stderr = self.err)
-        print( 'exit_code: {0}'.format(str(exit_code)))
+        self.out    = ""
+        self.err    = ""
+        print(self._call_args)
+        exit_code   = subprocess.call(["python.exe", self._call_args[0]])
+        print('exit_code: {0}'.format(str(exit_code)))
         print('stdout: {0}\nstderr: {1} '.format(str(self.out), str(self.err)))
