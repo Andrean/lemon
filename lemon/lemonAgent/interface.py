43274f66a4afd0717a6b40d5e84c4af830659c2d
andrean <andreanpal@gmail.com>
2013-07-19 16:19:43 +0600

    Changed XMLRPC Client. I did  a queue of requests. Now all requests sended in single thread. This is needed for exclusion of errors such Idle and Request-Sent

--- lemon/lemonAgent/interface.py
+++ lemon/lemonAgent/interface.py
@@ -9,13 +9,13 @@ import json
 import xmlrpc.client
 import lemon
 import core
+import queue
 
 
 ERROR_NOT_IDENTIFIED  = '0000001'
 ERROR_NOT_AUTHORIZED  = '0000002'
 
 TASK_SUCCESSFULLY_ADDED = '11'
-
 
 
 class XMLRPC_Client(lemon.BaseAgentLemon):
@@ -25,14 +25,12 @@ class XMLRPC_Client(lemon.BaseAgentLemon):
 
 
     def __init__(self, _logger, _config, _info):
-        self._logger    = _logger
-        self._config    = _config
-        self._running   = False
         self._agentID   = None
         self._taskManager   = None
         self._server_addr   = _config['xmlrpc_server_addr']
         self._server_port   = _config['xmlrpc_server_port']
         self._last_refresh  = 0
+        self._reqQueue  = queue.Queue()
         lemon.BaseAgentLemon.__init__(self,_logger,_config, _info)
         
     def run(self):
@@ -42,10 +40,47 @@ class XMLRPC_Client(lemon.BaseAgentLemon):
         self._setReady()
         
         while(self._running):
+            self._process()
             time.sleep(0.01)
             
+    def _process(self):
+        req = self._reqQueue.get()
+        method  = req['method']
+        args    = req['args']
+        callback    = req['callback']
+        if callback:
+            result  = method(*args)
+            callback(result)
+        req['ready']    = True
+    
+    def _put(self, method, args=None, callback=None):
+        request  = {'method': method, 'args': args, 'callback':callback, 'ready': False}
+        self._reqQueue.put(request)
+        return request
+    
+    def _formRequest(self, method, *args):
+        result  = {'return': None}
+        def callback(_result):
+            result['return']   = _result
+        p_task  = self._put(method, args, callback)
+        print('BEGIN_' + str(p_task))
+        while p_task['ready'] is not True:
+            time.sleep(0.01)
+        print('END_' + str(p_task))
+        return result['return']
+        
+        
+    def _reqREFRESH(self, agentID):
+        return self._formRequest(self._connection.refresh, agentID)
+        
+    def _reqGET(self, agentID, key):
+        return self._formRequest(self._connection.get, agentID, key)
+    
+    def _reqPOSTDATA(self, agentID, json_data):
+        return self._formRequest(self._connection.postData, agentID, json_data)
+        
     def post(self, data):
-        code    = self._connection.postData(self._agentID, json.dumps(data))
+        code    = self._reqPOSTDATA(self._agentID,  json.dumps(data))
         if code is TASK_SUCCESSFULLY_ADDED:
             return True
         return False
@@ -54,7 +89,7 @@ class XMLRPC_Client(lemon.BaseAgentLemon):
         try:
             if key is None:
                 try:
-                    result  = self._connection.refresh(self._agentID)
+                    result = self._reqREFRESH(self._agentID)
                     print(result)
                     if result - self._last_refresh > 0:
                         self._last_refresh  = result
@@ -62,7 +97,7 @@ class XMLRPC_Client(lemon.BaseAgentLemon):
                 except Exception as e:
                     self._logger.exception(e)                               
             else:
-                result  = json.loads(self._connection.get(self._agentID, key))
+                result  = json.loads(self._reqGET(self._agentID, key))
                 return result
         except Exception as e:
             self._logger.exception(e)
