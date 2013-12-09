1f23f9c36ab3dd499de58a83fe3d86c61a3fdcbb
Andrean <andreanpal@gmail.com>
2013-12-09 19:16:29 +0600

    Added analize distr

--- lemon/lemonServer/controllers/webController.py
+++ lemon/lemonServer/controllers/webController.py
@@ -4,9 +4,12 @@
 #
 import core
 import os
+import json
+import re
+from webpersonal.update import analize_distr 
 
 def upload( req, res ):
-    #em  = core.getCoreInstance().getInstance('ENTITY_MANAGER')
+    em  = core.getCoreInstance().getInstance('ENTITY_MANAGER')
     content_disposition = req.headers.get('Content-Disposition','').split(';')
     disposition_type    = content_disposition[0]
     file    = {}
@@ -22,22 +25,36 @@ def upload( req, res ):
         return
     length  = int(req.headers.get('Content-Length',0))
     if 'filename' in file.keys():
-        os.makedirs('files', exist_ok=True)
-        with open('files/' + file['filename'], 'wb') as wstream:
-            buf_length  = 40000
-            while length > 0:
-                write_len   = buf_length if length > buf_length else length
-                buffer  = req.rfile.read( write_len )
-                wstream.write(buffer)
-                length -= buf_length
-            wstream.close()
+        em.fileManager.writeFile(file['filename'], req.rfile, length)
         res.send_response(201)
         res.end_headers()
-        res.wfile.write("")
+        res.wfile.write("")    
     else:
         res.send_response(406) 
         res.end_headers()  
-        res.wfile.write("")  
+        res.wfile.write("")
+        
+def post_distr( req, res ):
+    em  = core.getCoreInstance().getInstance('ENTITY_MANAGER')    
+    content_type    = req.headers.get('Content-Type','text/plain;charset=utf-8')
+    content_length  = int(req.headers.get('Content-Length',0))
+    if content_length == 0:
+        res.send_error(411)
+        return      
+    charset         = 'utf-8'
+    match = re.match(r';charset=([\w-]+)',content_type)
+    if match:   
+        charset = match.groups[1]        
+    body    = json.loads(str(req.rfile.read(content_length), charset))
+    if 'distr' in body.keys():        
+        distr_name  = body['distr']['name']
+        if em.fileManager.isExistsFile( distr_name ):
+            # Запускаем процедуру проверки дистрибутива
+            # после ее выполнения отправляем json с результатами
+            result  = analize_distr( em.fileManager.getFilePath( distr_name ) )
+            res.send_json( {'status': True, 'date': result} )
+        else:
+            res.send_error(404)            
               
 def test( req, res):
     res.send_content('test')
