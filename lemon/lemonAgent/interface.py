dbf8f45b9a27763521fa7c319651479dc4bc3f9f
Andrean <andreanpal@gmail.com>
2013-12-18 17:55:47 +0600

    Изменен таймаут ожидания соединения - 5 секунд В методе get_file добавлена проверка на отсутствие res

--- lemon/lemonAgent/interface.py
+++ lemon/lemonAgent/interface.py
@@ -55,9 +55,11 @@ class HTTPClient(lemon.BaseAgentLemon):
             callback(None, response)
         except queue.Empty:
             return
-        except (ConnectionError,ConnectionRefusedError, socket.timeout) as e:        
-            self._logger.exception(e)
-            callback(e)        
+        except (ConnectionError, socket.timeout, http.client.HTTPException):        
+            callback(sys.exc_info()[1],_reconnect=True)
+        except:
+            callback(sys.exc_info()[1])
+            raise        
             
     def putRequest(self, connection, request, callback):
         while True:
@@ -75,32 +77,46 @@ class   RequestHandler(object):
         self.client     = client
         self.connection = http.client.HTTPConnection(self.client.serverEndpoint[0], self.client.serverEndpoint[1],timeout=5)
         self.mutex  = threading.Lock()
-        if self.connect():        
-            self.client._logger.info('Connection to {0}:{1} sucessfully established'.format(str(self.client.serverEndpoint[0]), str(self.client.serverEndpoint[1])))        
+        self.connected = threading.Event()   
+        self.connect()             
     
     def request(self, method, path, body=None, headers={}):
-        response    = {'result': None, 'error': None, 'ready': threading.Condition(self.mutex)}
+        if self.connected.wait(5) is False:     # максимальное время ожидания запроса. Если за это время не соединилось - послать запрос к черту
+            return
+        response    = {'result': None, 'error': None, 'ready': threading.Condition(self.mutex), 'reconnect': False}
         agentID       = core.getCoreInstance().getItem('agent')['__id']
         headers['Lemon-Agent-ID']   = agentID
-        def callback( _error, _response=None ):
-            response['result']    = _response  
+        def callback( _error=None, _response=None, _reconnect = False ):
+            response['result']   = _response 
+            response['error']    = _error 
+            response['reconnect']= _reconnect
             with response['ready']:           
                 response['ready'].notify()
-        if not self.connecting:
-            self.client.putRequest( self.connection, [method, path, body, headers], callback )
-            with response['ready']:
-                response['ready'].wait()
-            if response['error']:
-                self.client._logger.exception(response['error'])
+                
+        self.client.putRequest( self.connection, [method, path, body, headers], callback )
+        with response['ready']:
+            response['ready'].wait()
+        print(response)
+        if response['error']:
+            self.client._logger.error( response['error'] )                
+            if response['reconnect']:
                 self.connect()
+            return                
         return response['result']
         
     def connect(self):
-        self.connecting = True
+        t   = threading.Thread(None, self.__connect)
+        t.setDaemon(True)
+        t.start()
+    
+    def __connect(self):        
+        self.connected.clear()
+        self.connection.close()        
         while self.client.is_alive():
             try:
                 self.connection.connect()
-                self.connecting = False
+                self.connected.set()        
+                self.client._logger.info('Connection to {0}:{1} sucessfully established'.format(str(self.client.serverEndpoint[0]), str(self.client.serverEndpoint[1])))
                 return True
             except ConnectionError:
                 self.client._logger.error('Connection to {0}:{1} failed. Reconnecting after {2} seconds'.format(self.client.serverEndpoint[0],
@@ -108,8 +124,9 @@ class   RequestHandler(object):
                                                                                                      str(RECONNECTION_INTERVAL)
                                                                                                      )
                                    )
+                
                 time.sleep(RECONNECTION_INTERVAL)
-        return 
+        return
     
     def send_json(self, obj, url, headers={}):
         body    = json.dumps(obj)
@@ -124,9 +141,27 @@ class   RequestHandler(object):
         if 'Content-Type' not in headers.keys():
             headers['Content-Type'] = 'text/plain' + ';charset={0}'.format(encoding)
         return self.request('POST', url, body, headers)
+    
+    def send_content(self, binary_data, url, headers={}):
+        body    = binary_data
+        headers['Content-Length']   = len(body)
+        if 'Content-Type' not in headers.keys():
+            headers['Content-Type'] = 'application/octet-stream'
+        return self.request('POST', url, body, headers)
         
     def get_content(self, url, headers={}):
         return self.request('GET', url, "", headers)
+    
+    def get_file(self, url, headers={}):
+        res = self.request('GET', url, "", headers)
+        
+        if res is None:
+            return
+        length      = res.headers.get('Content-Length',0)
+        content_d  =  res.headers.get('Content-Disposition','')
+        _type, _options = content_d.split(';')
+        filename    = _options.split('filename=')[1].replace('"','')
+        return { 'file': res, 'type': _type, 'filename': filename, 'length': int(length) }
     
     def close(self):
         self.connection.close()
