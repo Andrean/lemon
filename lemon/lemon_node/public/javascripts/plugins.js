74fe0700fa788f768759effb379c80e4ca6f1c1c
Andrean <andreanpal@gmail.com>
2014-02-07 15:28:15 +0600

    Fixed some bugs

--- lemon/lemon_node/public/javascripts/plugins.js
+++ lemon/lemon_node/public/javascripts/plugins.js
@@ -2,27 +2,26 @@
  * 		Page Configuration - Plugins
  */
 !function($){
-	// TODO: Add animation
 	function modern_switch_tab( options, element ){
 		var $target	= $(options.target);
 		var $container	= $(options.container);
-		$container.children(':not('+options.target+')').hide('slide',{direction: 'left'}, 300, function(){
-			$target.show('slide',{direction: 'right'},300);
-		});
-				
+		if($target.is(':hidden')){
+			$container.children(':not('+options.target+')').hide('slide',{direction: 'left'}, 300, function(){
+				element.attr('data-switched','true');
+				$target.show('slide',{direction: 'right'},300);
+			});				
+		}
 	}
-	// TODO: Add animation
 	function backward_tab( options, element ){
 		var $target	= $(options.target);
 		$(options.hide).hide('slide',{direction: 'left'}, 300, function(){
 			$target.show('slide',{direction: 'right'},300);
-		});
-		
+		});		
 	}
 	$(document).on('click','[data-role="modern-switch-tab"]', function(){
 		var $this	= $(this);
 		var target	= $this.attr('data-target');
-		var container	= $this.attr('data-container');
+		var container	= $this.attr('data-container');		
 		modern_switch_tab({ target: target, container: container }, $this);
 		return false;
 	});
